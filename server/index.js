dd1f3f778a275e92f7e91ad25024bda42a72e058
Ohad Shalom <ohadshalom@Ohads-MacBook-Pro.local>
2023-02-10 10:20:54 +0200

    added dev support

--- server/index.js
+++ server/index.js
@@ -2,24 +2,28 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient({
+const redis_client = redis.createClient({
     host: 'redis-server',//The name of the redis container specified in docker-compose file
     port: 6379
 });
-client.set('visits',0)
+redis_client.set('visits',0)
 
-app.get("/",(req,res)=>{
-    console.log("/ reached");
-    client.get('visits',(err,visits)=>{
+
+app.get("/",async (req,res)=>{
+    redis_client.get("visits",(err,visits)=>{
+        console.log("/ reached");
         console.log('====================================');
         console.log("visits is "+visits);
         console.log('====================================');
         res.send("Number of visits is "+visits)
-        client.set('visits',parseInt(visits)+1)
+        redis_client.set('visits',parseInt(visits)+1)
     })
+    
 })
 
 const PORT = 8081
 app.listen(PORT,()=>{
     console.log("listening on port "+ PORT)
 })
+
+exports = app 
